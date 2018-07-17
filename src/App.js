20664920aa6b1e18dbc949ad3cce8845b9be1385
Varaprasad <varaprasad.babu5@gmail.com>
2018-07-17 12:59:09 +0530

    task2
    babu

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css'; 
+import ProductPannel from './components/abc/products'
+import ViewBill from './components/abc/view-bill'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//import BasicExample from './components/home/home'
+class App extends Component {
+  render() {
+    return (
+    
+      
+      <Router>
+      <div className="container">
+        <Route exact path="/" component={ProductPannel} />
+        <Route path="/ViewBill" component={ViewBill} />
+      </div>
+    </Router>
+        );
+  }
+}
+export default App;
